59cc9ad9ed95c90b4974786ecc73e2aa38962117
Simon Bernoud <simon.bernoud@gmail.com>
2021-10-12 17:30:40 +0200

    change app name

--- leQG/blog.py
+++ leQG/blog.py
@@ -3,8 +3,8 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from leQG.auth import login_required
+from leQG.db import get_db
 
 bp = Blueprint('blog', __name__)
 
@@ -94,4 +94,3 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
-
