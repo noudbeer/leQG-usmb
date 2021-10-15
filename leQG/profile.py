5d96f7622c152d20c2486b6775e0b3c635b27534
Simon Bernoud <simon.bernoud@gmail.com>
2021-10-15 14:06:17 +0200

    1st profile version

---
+++ leQG/profile.py
@@ -0,0 +1,39 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, url_for
+)
+from werkzeug.exceptions import abort
+
+from leQG.auth import login_required
+from leQG.db import get_db
+
+bp = Blueprint('profile', __name__)
+
+def get_user(id):
+    user = get_db().execute(
+        'SELECT u.id, username'
+        ' FROM user u'
+        ' WHERE u.id = ?',
+        (id,)
+    ).fetchone()
+
+    if user is None:
+        abort(404, f"user id {id} doesn't exist.")
+
+    return user
+
+
+@bp.route('/profile/<int:id>', methods=('GET',))
+def index(id):
+    db = get_db()
+
+    user = get_user(id)
+
+    posts = db.execute(
+        'SELECT p.id, title, body, created, author_id'
+        ' FROM post p'
+        ' WHERE p.author_id = ?',
+        (id,),
+        # ' ORDER BY created DESC' 
+    ).fetchall()
+
+    return render_template('user/profile.html', user=user, posts=posts)
